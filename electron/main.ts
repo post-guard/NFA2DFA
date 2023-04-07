ddd0829609eb6b1ca2b6927e83af0e2a48697a25
ichirinko <1621543655@qq.com>
2023-04-07 22:23:42 +0800

    add:绘制了初步的界面

--- electron/main.ts
+++ electron/main.ts
@@ -3,14 +3,19 @@ import {app, BrowserWindow} from 'electron'
 function createMainWindow() {
     const window = new BrowserWindow({
         title: "MainWindow",
-        width: 800,
-        height: 600
+        width: 1080,
+        height: 720,
+        useContentSize: true, //这个设置为真时，设置的尺寸将会是不包括菜单栏的窗口大小
     });
 
     if (process.env.VITE_DEV_SERVER_URL) {
         window.loadURL(process.env.VITE_DEV_SERVER_URL);
     } else {
         window.loadFile("dist/index.html");
+    }
+
+    if (!app.isPackaged) {
+        window.webContents.openDevTools();
     }
 }
 
---
+++ src/views/WorkPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="workPage">
+    <div class="DFABox">
+
+      <div class="DFADivideLine">
+        <a-divider style="border-color: #181818" >
+          <h2>
+            DFA
+          </h2>
+
+        </a-divider>
+      </div>
+
+      <div class="verticalDivider">
+        <a-divider type="vertical" style="border-color: #181818">
+aa
+        </a-divider>
+      </div>
+
+
+    </div>
+
+    <div class="NFABox">
+      <div class="NFADivideLine">
+        <a-divider style="border-color: #181818" >
+          <h2>
+            NFA
+          </h2>
+
+        </a-divider>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const DFA_Q = ref("");
+</script>
+
+<style scoped>
+.workPage{
+  position: absolute;
+  width: 1080px;
+  height: 720px;
+
+
+}
+
+.DFABox{
+  position: absolute;
+  width: 540px;
+  height: 720px;
+  top: 0px;
+  left: 0px;
+
+}
+
+.DFADivideLine{
+
+
+  position: absolute;
+
+  width: 540px;
+  height: 40px;
+  top: 360px;
+  left: 0px;
+
+}
+
+.NFABox{
+  position: absolute;
+  width: 540px;
+  height: 720px;
+  top: 0px;
+  left: 540px;
+
+}
+
+.NFADivideLine{
+
+
+  position: absolute;
+
+  width: 540px;
+  height: 40px;
+  top: 360px;
+  left: 0px;
+
+}
+
+.verticalDivider{
+  position: absolute;
+
+  height: 400px;
+  width: 60px;
+  top: 20px;
+  left: 400px;
+}
+</style>
