8f1929baa7a37f23b426355919f36c574ad1ab39
jackfiled <xcrenchangjun@outlook.com>
2023-04-11 10:21:08 +0800

    feature: 修改了DFA和NFA的模型设计

--- src/models/DFA.ts
+++ src/models/DFA.ts
@@ -1,18 +1,17 @@
 import {State} from "@/models/State";
 import type {InputItem} from "@/models/InputItem";
-import type {Pair} from "@/models/Pair";
 
 export class DFA {
     public states : Set<State>;
     public inputItems : Set<InputItem>;
-    public table : Map<Pair, State>;
+    public table : Map<State, Map<InputItem, State>>;
     public startState : State;
     public endStates: Set<State>;
 
     public constructor() {
         this.states = new Set<State>();
         this.inputItems = new Set<InputItem>();
-        this.table = new Map<Pair, State>();
+        this.table = new Map<State, Map<InputItem, State>>();
         this.startState = new State("");
         this.endStates = new Set<State>();
     }
--- src/models/NFA.ts
+++ src/models/NFA.ts
@@ -1,18 +1,17 @@
 import {State} from "@/models/State";
 import type {InputItem} from "@/models/InputItem";
-import type {Pair} from "@/models/Pair";
 
 export class NFA {
     public states : Set<State>;
     public inputItems : Set<InputItem>;
-    public table : Map<Pair, Set<State>>;
+    public table : Map<State, Map<InputItem, Set<State>>>;
     public startState : State;
     public endStates: Set<State>;
 
     public constructor() {
         this.states = new Set<State>();
         this.inputItems = new Set<InputItem>();
-        this.table = new Map<Pair, Set<State>>();
+        this.table = new Map<State, Map<InputItem, Set<State>>>();
         this.startState = new State("");
         this.endStates = new Set<State>();
     }
